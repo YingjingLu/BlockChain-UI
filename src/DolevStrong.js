b5fb1a0ee9a130bf69c499c89ce807654fe070bc
YingjingLu <yingjinglu2019us@gmail.com>
2020-12-14 14:26:34 -0500

    d

--- src/DolevStrong.js
+++ src/DolevStrong.js
@@ -161,7 +161,7 @@ class DolevStrong extends React.Component {
     render_illustrations() {
         return (
             <>
-                <p>Cur Round Output Bit</p>
+                <p>Cur Round Output Bit</p>{' '}
                 <style type="text/css">
                     {`
                     .badge-flat {
